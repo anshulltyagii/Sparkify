b6b4e22c24f53261ff590bce5d7c5ae26874183a
anshulltyagii <21102086@mail.jiit.ac.in>
2024-06-11 15:03:12 +0530

    Signup Page Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+
+import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+
 const App = () => {
   return (
-    <div>App</div>
+   <main className="flex h-screen">
+    <Routes>
+      { /* public routes */}
+      <Route element={<AuthLayout />}>
+
+      <Route path="/sign-in" element={<SigninForm />} />
+      <Route path="/sign-up" element={<SignupForm />} />
+      </Route>
+
+      { /* private routes */}
+      <Route element={<RootLayout />}>
+      <Route index element={<Home />}/>
+      </Route>
+    </Routes>
+   </main>
   )
 }
 
