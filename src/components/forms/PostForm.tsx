0b2caad05948f5164b988f6343198cc261919aa3
anshulltyagii <21102086@mail.jiit.ac.in>
2024-06-23 19:37:26 +0530

    chore: Update PostForm component with validation and submit logic

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -1,33 +1,31 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
- 
-
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
+import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage,} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
+import { PostValidation } from "@/lib/validation"
+import { Models } from "appwrite"
+import { useUserContext } from "@/context/AuthContext"
+import { toast, useToast } from "../ui/use-toast"
+import { useNavigate } from "react-router-dom"
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+type PostFormProps ={
+  post?: Models.Document;
+}
 
-const PostForm = ({ post }) => {
+const PostForm = ({ post }: PostFormProps) => {
+
+const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost();
+const { user } = useUserContext();
+const { toast } = useToast();
+const navigate = useNavigate();
 
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof PostValidation>>({
+    resolver: zodResolver(PostValidation),
     defaultValues: {
       caption: post ? post?.caption : "",
       file: [],
@@ -38,10 +36,21 @@ const PostForm = ({ post }) => {
    })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof PostValidation>) {
+    const newPost = await createPost({
+      ...values,
+      userId: user.id,
+    })
+
+    if(!newPost){
+      toast({
+        title: 'Please try again'
+      })
+    }
+
+    navigate('/');
+
+
   }
 
   return (
@@ -83,7 +92,7 @@ const PostForm = ({ post }) => {
             <FormItem>
               <FormLabel className="shad-form_label">Add Location</FormLabel>
               <FormControl>
-                <Input type="text" className="shad-input"/>
+                <Input type="text" className="shad-input" {...field}/>
               </FormControl>
               <FormMessage className="shad-form_message"/>
             </FormItem>
@@ -98,7 +107,9 @@ const PostForm = ({ post }) => {
               <FormControl>
                 <Input type="text" 
                       className="shad-input" 
-                      placeholder="Art, Expression, Learn"/>
+                      placeholder="Art, Expression, Learn"
+                      {...field}
+                      />
               </FormControl>
               <FormMessage className="shad-form_message"/>
             </FormItem>
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,6 +1,5 @@
 import { z } from "zod"
 
-
 export const SignupValidation = z.object({
     name: z.string().min(2, {message: 'Too short'}),
     username: z.string().min(2, {message: 'Too Short' }),
@@ -10,4 +9,11 @@ export const SignupValidation = z.object({
 export const SigninValidation = z.object({
     email: z.string().email(),
     password: z.string().min(8, {message: 'Password must be atleast 8 characters long'}),
+  })
+
+  export const PostValidation = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string(),
   })
