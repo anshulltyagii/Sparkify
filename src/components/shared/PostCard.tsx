e34d6422f5d34e4719c1ba37199c78ce3e5b95a1
anshulltyagii <21102086@mail.jiit.ac.in>
2024-06-30 11:28:01 +0530

    feat: Add lazy loading for post images and improve post details page layout

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -16,7 +16,7 @@ const PostCard = ({post}: PostCardProps) => {
     <div className="post-card">
         <div className="flex-between">
             <div className="flex items-center gap-3">
-                <Link to={'/profile/${post.creator.$id'}>
+                <Link to={`/profile/${post.creator.$id}`}>
                     <img 
                         src={post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg'}
                         alt = 'creator'
